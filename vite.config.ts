75994aa8e6ae4599dc5d6051b266a4665629dd22
Hm255 <hamza.moosa@outlook.com>
2025-06-10 15:45:23 +0100

    vite.config/ts rewrite to fix render hosting issues

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,23 @@
-
-import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
-import path from 'path';
-
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import path from "path";
 
 export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
+      "@": path.resolve(__dirname, "./src"),
     },
+  },
+  build: {
+    outDir: "frontend", // or any directory that matches your hosting settings
   },
   server: {
     proxy: {
-      '/api': {
-        target: 'http://localhost:9090',
+      "/api": {
+        target: "http://localhost:9090",
         changeOrigin: true,
-      }
+      },
     },
   },
 });
